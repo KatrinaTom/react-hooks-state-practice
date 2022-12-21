003234e3b254070d3e3db74e3d049ad036eb3a52
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2022-12-21 10:28:38 +1000

    react hooks state exercise completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Heading} from './Styled'
 import NewProjectForm from './NewProjectForm'
 import ProjectList from './ProjectList'
 
 const App = () => {
-	const projects = [
-		{
-			name: "Sets",
-			description: "A React version of the popular logic game"
-		},
-		{
-			name: "Chore Tracker",
-			description: "A full stack app to track chores completed and money earned"
-		},
-		{
-			name: "Fotozu",
-			description: "A full stack app using AWS lambda for serverless backend that showcases the photography of a client"
-		}
-	]
+	const initialProjects = []
+	function addProject(project) {
+		setProjects([project, ...projects])
+	}
+
+	const [projects, setProjects] = useState(initialProjects)
+
+	useEffect(() => {
+		console.log("in use effect")
+		fetch('project.json',{
+			headers : {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			}
+		})
+		.then((response) => response.json())
+		.then((projects) => setProjects(projects))
+	}, [])
+
 	return (
 		<div >
 			<Heading>Recent Projects</Heading>
-			<NewProjectForm />
+			<NewProjectForm addProject={addProject}/>
 			<ProjectList projects={projects} />
 		</div>
 	)
--- src/NewProjectForm.js
+++ src/NewProjectForm.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {TextField, Button, Row, RightRow} from './Styled'
 
-export default function NewProjectForm() {
+export default function NewProjectForm(props) {
+	const {addProject} = props
+	const initialFormState = {
+		name: "",
+		description: ""
+	}
+	const [formData, setFormData] = useState(initialFormState)
+
+	function changeHandler(event) {
+		setFormData({
+			...formData, 
+			[event.target.name]: event.target.value
+		})
+	}
+
+	function submitHandler(event) {
+		event.preventDefault()
+		addProject(formData)
+		setFormData({
+			name: "",
+			description: ""
+		})
+	}
+
 	return (
-		<form>
+		<form onSubmit={submitHandler}>
 			<Row>
 				<label>Name:</label>	
-				<TextField></TextField>
+				<TextField name="name" value={formData.name} onChange={changeHandler}></TextField>
 			</Row>
 			<Row>
 				<label>Description:</label>
-				<TextField></TextField>
+				<TextField name="description" value={formData.description} onChange={changeHandler}></TextField>
 			</Row>
 			<RightRow>
 				<Button>Add Project</Button>
 			</RightRow>
 		</form>
 	)
+	
 }
